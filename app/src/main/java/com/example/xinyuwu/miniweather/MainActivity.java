f638057ffc1ad0ce6592bb4443d41fd7a6d21ffd
rakycc <xinyu_wu@pku.edu.cn>
2018-10-11 16:47:25 +0800

    Internet Access

--- app/src/main/java/com/example/xinyuwu/miniweather/MainActivity.java
+++ app/src/main/java/com/example/xinyuwu/miniweather/MainActivity.java
@@ -1,13 +1,122 @@
 package com.example.xinyuwu.miniweather;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.example.xinyuwu.util.NetUtil;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+    private ImageView mUpdateBtn;
     @Override
-    protected void onCreate(Bundle savedInstance){
-    super.onCreate(savedInstance);
-    setContentView(R.layout.weather_info);
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.weather_info);
+
+        mUpdateBtn = (ImageView) findViewById(R.id.title_update_btn);
+        mUpdateBtn.setOnClickListener(this);
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myWeather", "网络正常");
+            Toast.makeText(MainActivity.this, "网络正常", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("myWeather", "请检查网络");
+            Toast.makeText(MainActivity.this, "请检查网络", Toast.LENGTH_SHORT).show();
+        }
     }
+    @Override
+    public void onClick(View view){
+            if(view.getId()==R.id.title_update_btn){
+                SharedPreferences sharedPreferences=getSharedPreferences("config",MODE_PRIVATE);
+                String cityCode=sharedPreferences.getString("main_city_code","101010100");
+                Log.d("myWeather",cityCode);
+                if(NetUtil.getNetworkState(this)!=NetUtil.NETWORN_NONE){
+                    Log.d ("myWeather","网络正常");
+                    queryWeatherCode(cityCode);
+                }else{
+                    Log.d("myWeather","请检查网络");
+                    Toast.makeText(MainActivity.this, "请检查网络", Toast.LENGTH_SHORT).show();
+                }
+            }
+    }
+   private void queryWeatherCode(String cityCode) {
+       final String address = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + cityCode;
+       Log.d("myWeather", address);
+       new Thread(new Runnable() {
+           @Override
+           public void run() {
+               HttpURLConnection con = null;
+               try {
+                   URL url = new URL(address);
+                   con = (HttpURLConnection) url.openConnection();
+                   con.setRequestMethod("GET");
+                   con.setConnectTimeout(8000);
+                   con.setReadTimeout(8000);
+                   InputStream in = con.getInputStream();
+                   BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                   StringBuilder response = new StringBuilder();
+                   String str;
+                   while ((str = reader.readLine()) != null) {
+                       response.append(str);
+                       Log.d("myWeather", str);
+                   }
+                   String responseStr = response.toString();
+                   Log.d("myWeather", responseStr);
+                   parseXML(responseStr);
+               } catch (Exception e) {
+                   e.printStackTrace();
+               } finally {
+                   if (con != null) {
+                       con.disconnect();
+                   }
+               }
+           }
+       }).start();
+   }
+   private void parseXML(String xmldata){
+        try{
+            XmlPullParserFactory fac=XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser=fac.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmldata));
+            int eventType=xmlPullParser.getEventType();
+            Log.d("myWeather","parseXML");
+            while(eventType!=XmlPullParser.END_DOCUMENT){
+                switch(eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    case XmlPullParser.START_TAG:
+                        if (xmlPullParser.getName().equals("city")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "city: " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("updatetime")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "updatetime: " + xmlPullParser.getText());
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        break;
+                }
+                eventType=xmlPullParser.next();
+                }
+            }catch(XmlPullParserException e) {
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+   }
 }
